71de069d9e3aa464ebd79e5cdb4f34ce6487d397
davematosyan <dvdmatosyan@gmail.com>
2025-03-20 00:20:47 +0400

    adding reset password without propper alert handling

---
+++ pages/Auth/ForgotPasswordScreen.jsx
@@ -0,0 +1,126 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert, ActivityIndicator, Dimensions } from 'react-native';
+import axios from 'axios';
+import { BASE_URL } from '../../utils/requests';
+import { Image } from 'react-native-elements';
+import SuccessAlert from '@/components/SuccessAlert';
+
+const { width, height } = Dimensions.get('window');
+
+const ForgotPasswordScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [visible, setVisible] = useState(false);
+
+  const handleSendCode = async () => {
+    if (!email) {
+      Alert.alert('Error', 'Please enter your email address.');
+      return;
+    }
+
+    setLoading(true);
+    try {
+      await axios.get(`${BASE_URL}/api/v1/Authentication/forget-password/send-email?email=${email}`);
+
+      setVisible(true)
+
+    } catch (error) {
+
+      Alert.alert('Error', error.response.data.message || 'Failed to send verification code.');
+      
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+    <SuccessAlert visible={visible}
+    onCancel={() => navigation.navigate('LoginScreen')}
+    
+    />
+      <Image
+        source={require('../../assets/images/trueLogo.png')}
+        style={styles.logo}
+        resizeMode="contain"
+      />
+      <Text style={styles.title}>Forgot Password</Text>
+      <Text style={styles.subtitle}>Enter your email to receive a verification code.</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        placeholderTextColor="#aaa"
+        keyboardType="email-address"
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize="none"
+      />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={handleSendCode}
+        disabled={loading}
+      >
+        {loading ? (
+          <ActivityIndicator size="small" color="#000" />
+        ) : (
+          <Text style={styles.buttonText}>Send Verification Code</Text>
+        )}
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#1C1C1C',
+    padding: 20,
+  },
+  title: {
+    fontFamily: 'RobotoBold',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#fff',
+  },
+  subtitle: {
+    fontFamily: 'RobotoRegular',
+    fontSize: 16,
+    marginBottom: 20,
+    color: '#aaa',
+    textAlign: 'center',
+  },
+  input: {
+    height: 50,
+    borderColor: '#ccc',
+    borderWidth: 0.5,
+    marginBottom: 15,
+    paddingHorizontal: 10,
+    backgroundColor: '#2E2E2E',
+    borderRadius: 10,
+    color: '#fff',
+    width: '80%',
+  },
+  button: {
+    backgroundColor: '#fff',
+    padding: 15,
+    alignItems: 'center',
+    borderRadius: 10,
+    width: '80%',
+  },
+  buttonText: {
+    color: '#000',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  logo: {
+    width: width * 0.8,
+    height: height * 0.33,
+    marginBottom: -height * 0.01,
+    marginLeft: width * 0.02,
+  },
+});
+
+export default ForgotPasswordScreen;
